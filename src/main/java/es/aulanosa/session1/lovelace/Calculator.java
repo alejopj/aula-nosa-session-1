9108fd102365686b89a20e365e0bd4d3e07c1396
alejopj <alejopj@gmail.com>
2021-10-31 12:51:23 +0100

    Add exercise solutions

--- src/main/java/es/aulanosa/session1/lovelace/Calculator.java
+++ src/main/java/es/aulanosa/session1/lovelace/Calculator.java
@@ -7,50 +7,93 @@ public class Calculator {
 
 	public Double add(Double a, Double b) {
 		
-		// TODO Your code goes here.
-		return null;
+		Double safeA = getSafeOperand(a);
+		Double safeB = getSafeOperand(b);
+		
+		return safeA + safeB;
 	}
 	
 	public Double subtract(Double a, Double b) {
 		
-		// TODO Your code goes here.
-		return null;
+		Double safeA = getSafeOperand(a);
+		Double safeB = getSafeOperand(b);
+		
+		return safeA - safeB;
 	}
 	
 	public Double multiply(Double a, Double b) {
 		
-		// TODO Your code goes here.
-		return null;
+		Double safeA = getSafeOperand(a);
+		Double safeB = getSafeOperand(b);
+		
+		return safeA * safeB;
 	}
 	
 	public Double divide(Double a, Double b) {
 		
-		// TODO Your code goes here.
-		return null;
+		Double safeA = getSafeOperand(a);
+		Double safeB = getSafeOperand(b);
+		
+		if (safeA.equals(0.0) && safeB.equals(0.0)) {
+			return null;
+		} else if (safeB.equals(0.0)) {
+			return Double.MAX_VALUE;
+		}
+		
+		return safeA / safeB;
 	}
 	
 	public List<Double> add(List<Double> a, List<Double> b) {
 		
-		// TODO Your code goes here.
-		return new ArrayList<>();
+		List<Double> c = new ArrayList<>();
+		
+		for (int i = 0; i < a.size(); i++) {
+			
+			c.add(add(a.get(i), b.get(i)));
+		}
+		
+		return c;
 	}
 	
 	public List<Double> subtract(List<Double> a, List<Double> b) {
 		
-		// TODO Your code goes here.
-		return new ArrayList<>();
+		List<Double> c = new ArrayList<>();
+		
+		for (int i = 0; i < a.size(); i++) {
+			
+			c.add(subtract(a.get(i), b.get(i)));
+		}
+		
+		return c;
 	}
 	
 	public List<Double> multiply(List<Double> a, List<Double> b) {
 		
-		// TODO Your code goes here.
-		return new ArrayList<>();
+		List<Double> c = new ArrayList<>();
+		
+		for (int i = 0; i < a.size(); i++) {
+			
+			c.add(multiply(a.get(i), b.get(i)));
+		}
+		
+		return c;
 	}
 	
 	public List<Double> divide(List<Double> a, List<Double> b) {
 		
-		// TODO Your code goes here.
-		return new ArrayList<>();
+		List<Double> c = new ArrayList<>();
+		
+		for (int i = 0; i < a.size(); i++) {
+			
+			c.add(divide(a.get(i), b.get(i)));
+		}
+		
+		return c;
+	}
+	
+	private Double getSafeOperand(Double operand) {
+		
+		return operand != null ? operand : 0.0;
 	}
 	
 }
--- src/main/java/es/aulanosa/session1/turing/Enigma.java
+++ src/main/java/es/aulanosa/session1/turing/Enigma.java
@@ -11,14 +11,59 @@ public class Enigma {
 
 	public String encrypt(String text, Integer rotation) {
 		
-		// TODO Your code goes here.
-		return null;
+		String encryptedText = "";
+		
+		if (alphabet == null || text == null || !containsAll(alphabet, text) || rotation == null) {
+			return text;
+		}
+		
+		for (int i = 0; i < text.length(); i++) {
+			
+			char textChar = text.charAt(i);
+			int alphabetIndex = alphabet.indexOf(textChar);
+			char encryptedTextChar = alphabet.charAt((alphabetIndex + rotation) % alphabet.length());
+			encryptedText = encryptedText.concat("" + encryptedTextChar);
+		}
+		
+		return encryptedText;
 	}
 	
 	public String decrypt(String text, Integer rotation) {
 		
-		// TODO Your code goes here.
-		return null;
+		String decryptedText = "";
+		
+		if (alphabet == null || text == null || !containsAll(alphabet, text) || rotation == null) {
+			return text;
+		}
+		
+		for (int i = 0; i < text.length(); i++) {
+			
+			char textChar = text.charAt(i);
+			int alphabetIndex = alphabet.indexOf(textChar);
+			char encryptedTextChar = alphabet.charAt((alphabetIndex - rotation + alphabet.length()) % alphabet.length());
+			decryptedText = decryptedText.concat("" + encryptedTextChar);
+		}
+		
+		return decryptedText;
+	}
+	
+	private boolean containsAll(String alphabet, String text) {
+		
+		char[] alphabetCharacters = alphabet.toCharArray();
+		char[] textCharacters = text.toCharArray();
+		int count = 0;
+		
+		for (int i = 0; i < textCharacters.length; i++) {
+			for (int j = 0; j < alphabetCharacters.length; j++) {
+				
+				if (alphabetCharacters[j] == textCharacters[i]) {
+					count++;
+					break;
+				}
+			}
+		}
+		
+		return count == textCharacters.length;
 	}
 	
 }
